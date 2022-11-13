798fc4f341d89f7aa4dbcd439e360d029ba860d5
Matt Meents <mmeents@users.noreply.github.com>
2022-11-12 21:30:12 -0500

    Add some CI and test project

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestStaticExtensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ReUse Computers")]
+[assembly: AssemblyProduct("TestStaticExtensions")]
+[assembly: AssemblyCopyright("Copyright © ReUse Computers 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("765b8e1c-4ab2-494d-bf00-9a85065c77ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
