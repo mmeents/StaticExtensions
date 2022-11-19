bbab700d971a4797c52efbb5bb292f9cb182207b
Matt Meents <mmeents@users.noreply.github.com>
2022-11-18 20:41:52 -0500

    Change Target Deployment, update CI

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,14 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StaticExtensions")]
+
 [assembly: AssemblyDescription("Library of common exts")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sky High Productions, LLC")]
-[assembly: AssemblyProduct("StaticExtensions")]
-[assembly: AssemblyCopyright("Copyright © 2022 Sky High Productions, LLC")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+// [assembly: AssemblyVersion("1.0.3")]
+// [assembly: AssemblyFileVersion("1.0.3")]
